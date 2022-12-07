054c399b6057893b3bf0c73ada03aa791668db66
Cleiton Carvalho <cleitoncosta83@gmail.com>
2022-12-07 13:12:15 -0300

    feat: Added DateHead template

---
+++ src/components/Chat/DateHead.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="date-head q-pa-md q-gutter-sm items-center justify-center row">
+    <div class="line col-12"></div>
+    <div class="text text-subtitle2 text-teal-1 shadow-2 rounded-borders">
+      Saturday, 28th
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'DateHead',
+});
+</script>
